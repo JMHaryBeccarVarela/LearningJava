38dc54cb237868e22c5ef01c51b6f360db5fe4c5
JMHaryBeccarVarela <julianhary04@gmail.com>
2023-09-01 10:52:08 -0300

    My first Java commit!

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World, I am coding with Java!");
+
+        String name = "Julián";
+        System.out.println("Hello World, I am " + name + "!");
+    }
+}
