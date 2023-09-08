30a7c63a317ddce97758aa3b216adc953a44685b
JMHaryBeccarVarela <julianhary04@gmail.com>
2023-09-08 12:39:37 -0300

    learning about variables definition

--- src/Variables.java
+++ src/Variables.java
@@ -18,5 +18,24 @@ public class Variables {
 
         var technology = 10; //implicit value in primitives and references with var word//
         var Tech = 10;
+
+        // avoid ñ and ´ in variable names
+
+        //PRIMITIVES:
+    //booleans
+        boolean a = true;
+        boolean b = false;
+    // char = one only character or unicode reference:
+        char c = 'a';
+        char d = '1';
+        char e = '\u0021'; // this equals the "!" symbol in the unicode
+    // enteros
+        byte f = 120; //between -128 - 127
+        short g = 32767; //between -32768 - 32767
+        int h = 1387483822;
+        long i = 217974793;
+
+    // reales = con coma
+        // float (debeen terminar con la letra "f" para menos rando y double para mas rango, se pueden escribir como potencias.
     }
 }
